2bb4fdc1474e4986a0c1dce5cc012e2d247aa224
J <3cgg@163.com>
2018-02-07 17:12:12 +0800

    -

---
+++ src/main/scala/scalalg/me/libme/apitable/dic/CodeModel.scala
@@ -0,0 +1,8 @@
+package scalalg.me.libme.apitable.dic
+
+/**
+  * Created by J on 2018/2/7.
+  */
+case class CodeModel(code:String,_type:String,name:String,desc:String) {
+
+}
---
+++ src/main/scala/scalalg/me/libme/apitable/dic/Model2DML.scala
@@ -0,0 +1,10 @@
+package scalalg.me.libme.apitable.dic
+
+/**
+  * Created by J on 2018/2/7.
+  */
+trait Model2DML {
+
+  def dml(codeModel: CodeModel):String;
+
+}
